7747e52f6eb19ee5054218eadc673478a2ee0f1f
Yongmin Cho <qlxhdl55@gmail.com>
2020-11-04 19:06:19 -0800

    update localhost name

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let app = express();
 let authRoutes = require('./routes/auth.js');
 let userRoutes = require('./routes/users.js');
 
-mongoose.connect('mongodb://localhost:27017/catholic', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/catholic', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
