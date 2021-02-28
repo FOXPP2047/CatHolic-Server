077ff47d79f3b907f77fa8e9156bd35552b73e71
Yongmin Cho <qlxhdl55@gmail.com>
2021-02-27 16:46:35 -0800

    if User logined already, redirect

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,9 +3,10 @@ let authMiddleware = {};
 authMiddleware.isLoggedIn = (req, res, next) => {
     if(req.isAuthenticated()) {
         return next();
+    } else {
+        console.log("user tried to access without being logged in");
+        res.send("user tried to access without being logged in");
     }
-    console.log("user tried to access without being logged in");
-    res.send("user tried to access without being logged in");
 }
 
 module.exports = authMiddleware;
